0a11f6cfe57839ab77cfb3d50c0f5e1b49b327f9
hiiamvalya <shekhovskaya.v@gmail.com>
2022-08-31 00:40:44 +0300

    init

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,8 @@
+public class FormDate {
+
+    public int day; // 13
+    public int month; // 6
+    public int year; // 1999
+
+}
+
---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+
+
+    public FormDate birthday;
+    public boolean subscription;
+    public String name; // Иван
+    public String passport; // 4444 № 44444444
+    public String patronymic; // Иванович
+    public String phone; // +7 (999)-999-99-99
+    public String surname; // Иванов
+
+}
